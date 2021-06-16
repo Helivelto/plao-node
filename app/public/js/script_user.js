efdf677681c2bd75a29ce86013f8bb2af509588d
Helivelto <helivelto.souza321@gmail.com>
2021-06-16 11:38:16 -0300

    arrumando cores do h3 e formatando codigo

--- app/public/js/script_user.js
+++ app/public/js/script_user.js
@@ -8,13 +8,11 @@ display_escritor.addEventListener('click', function(){
     usuarios_escritores.style.display = 'block'
     usuarios_leitores.style.display = 'none'
     
-    // display_escritor.classList.add('green')
-    // display_leitor.classList.remove('green')
-    // display_leitor.classList.add('black')
-
+    // mudando a cor do h3 usuarios escritores para verde
     display_escritor.classList.replace('black', 'green')
     display_escritor.classList.replace('non_border', 'border_red')
 
+    // mudando a cor do h3 usuarios leitores para preto
     display_leitor.classList.replace('green', 'black')
     display_leitor.classList.replace('border_red', 'non_border')
 })
@@ -23,13 +21,11 @@ display_leitor.addEventListener('click', function(){
     usuarios_escritores.style.display = 'none'
     usuarios_leitores.style.display = 'block'
 
-    // display_leitor.classList.add('green')
-    // display_escritor.classList.add('black')
-    // display_escritor.classList.remove('green')
-
+    // mudando a cor do h3 usuarios leitores para verde
     display_leitor.classList.replace('black', 'green')
     display_leitor.classList.replace('non_border', 'border_red')
 
+    // mudando a cor do h3 usuarios escritores para preto
     display_escritor.classList.replace('green', 'black')
     display_escritor.classList.replace('border_red', 'non_border')
     
