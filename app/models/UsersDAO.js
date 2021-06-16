26bc20350f83432ae1479e605219704a8bb71267
Helivelto <helivelto.souza321@gmail.com>
2021-06-16 14:23:24 -0300

    listando usuarios por ordem alfabetica

--- app/models/UsersDAO.js
+++ app/models/UsersDAO.js
@@ -3,7 +3,7 @@ function UsersDAO (connection) {
 }
 
 UsersDAO.prototype.getUsuarios = function (callback) {
-    this._connection.query('select * from usuarios', callback)
+    this._connection.query('select * from usuarios order by nome_usuario', callback)
 } 
 
 // UsersDAO.prototype.getUsuario = function (id_usuario, callback) {
