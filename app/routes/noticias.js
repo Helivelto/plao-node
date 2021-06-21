374db6320aaad1f17f9a28c75cc23753b72a231b
Helivelto <helivelto.souza321@gmail.com>
2021-06-21 16:19:54 -0300

    deletando pasta e rota admin e juntando com notcia

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -10,4 +10,14 @@ module.exports = (application) => {
         application.app.controllers.noticias.noticia(application, req, res)
     })
 
+    application.get('/formulario-inclusao-noticia', (req, res) => {
+        // res.render('admin/form_noticias', {validacao: {}, noticia: {}})
+        application.app.controllers.noticias.formulario_inclusao_noticia(application, req, res)
+    })
+
+    application.post('/noticias/salvar', (req, res) => {
+        application.app.controllers.noticias.noticias_salvar(application, req, res)
+    })
+
+
 }
